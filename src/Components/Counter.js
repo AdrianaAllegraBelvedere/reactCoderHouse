5a638f1037ce1f117597e0fc9fd6b1d36232428c
AdrianaAllegraBelvedere <adriana.allegrabelvedere@gmail.com>
2022-01-04 17:37:53 -0300

    fix desafio sincronizar counter

---
+++ src/Components/Counter.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react';
+
+const Counter =(props) => {
+
+    const {onAdd,initial,stock} = props;
+
+    const [contador,setContador] = useState(initial);
+
+    const Sumar = () =>{
+        let stockDisponible = stock > 0;
+        if(stockDisponible)
+        {
+            let conStock = contador + 1 <= stock;
+            if(conStock)
+              setContador(contador + 1);
+        }
+      }
+    
+      const Restar = () =>{
+
+        if(contador -1 >= 0)
+            setContador(contador - 1);
+
+      }
+
+    return(
+        <>
+            <form>
+                <div className="form-group mt-2 mb-1">
+                    <div className="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" className="btn btn-secondary" onClick={() => Restar()}>-</button>
+                        <button type="button" className="btn btn-secondary"><label className="text">{contador}</label></button>
+                        <button type="button" className="btn btn-secondary" onClick={() => Sumar()}>+</button>
+                    </div>
+                </div>
+                <button type="button" className="btn btn-primary mt-2 " onClick={() => onAdd(contador)}>Agregar</button>
+            </form>
+            <p className="mt-5">Tiene un stock de {stock} productos</p>
+        </>
+    )
+
+}
+export default Counter;
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
+import Counter from './Counter';
 
 const Item = ( {producto,setCantidadProductos} ) => {
 
@@ -7,7 +8,6 @@ const Item = ( {producto,setCantidadProductos} ) => {
     const stock = 5;
     const initial = 0;
 
-    const [contador,setContador] = useState(initial);
     
     const onAdd = (productosEnCarrito) =>{
         if(stock > 0)
@@ -19,30 +19,9 @@ const Item = ( {producto,setCantidadProductos} ) => {
                 console.log(`Tienes ${productosEnCarrito} productos en el carrito`)
                 setCantidadProductos(productosEnCarrito);
             }
-
         }
     }
 
-      
-    const Sumar = () =>{
-        let stockDisponible = stock > 0;
-        if(stockDisponible)
-        {
-            let conStock = contador + 1 <= stock;
-            if(conStock)
-              setContador(contador + 1);
-        }
-
-
-      }
-    
-      const Restar = () =>{
-
-        if(contador -1 >= 0)
-            setContador(contador - 1);
-
-      }
-      
     return(
         <>
             <div className="card product" >   
@@ -54,20 +33,9 @@ const Item = ( {producto,setCantidadProductos} ) => {
                     <p className="card-text">{description}</p>
                     <h6 className="card-subtitle mb-2 text-muted">$ {price}</h6>
                     <Link className="nav-link" to={`item/${id}`}>Mas detalles...</Link>
-                    <form>
-                        <div className="form-group mt-2 mb-1">
-                            <div className="btn-group" role="group" aria-label="Basic example">
-                                <button type="button" className="btn btn-secondary" onClick={() => Restar()}>-</button>
-                                <button type="button" className="btn btn-secondary"><label className="text">{contador}</label></button>
-                                <button type="button" className="btn btn-secondary" onClick={() => Sumar()}>+</button>
-                            </div>
-                        </div>
-                        <button type="button" className="btn btn-primary mt-2 " onClick={() => onAdd(contador)}>Agregar</button>
-                    </form>
-                    <p className="mt-5">Tiene un stock de {stock} productos</p>
+                    <Counter onAdd={onAdd} initial={initial} stock={stock}/>
                 </div>
             </div>
-                
         </>
     )
 }
